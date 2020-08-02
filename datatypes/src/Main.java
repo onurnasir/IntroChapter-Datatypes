8f0b1cb9fb7e315858fc0712092ca0584913653f
Onur Nasır <onur.nasr@gmail.com>
2020-08-02 18:18:08 +0300

    Datatypes example added.

---
+++ datatypes/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+	double sayi=12.5;
+	sayi=135;
+
+	System.out.println(sayi);
+
+	char karakter='A';
+	System.out.println(karakter);
+	//ascii kodu denilen her bir char değerinin sayısal bir karşılığı bulunmakdır.
+
+	boolean dogruMu=false;
+
+
+    }
+}
